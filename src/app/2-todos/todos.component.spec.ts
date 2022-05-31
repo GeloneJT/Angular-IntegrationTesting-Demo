3e6504352b6a0ecbbb5193d3be5c55bc66c5a115
Jelani <eyeglass0324@gmail.com>
2022-05-31 15:33:15 -0400

    finished demo to provide dependencies for integration tests

--- src/app/2-todos/todos.component.spec.ts
+++ src/app/2-todos/todos.component.spec.ts
@@ -2,25 +2,28 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
 
 //NOTE: I've deliberately excluded this suite from running
-// because the test will fail. This is because we have not 
-// provided the TodoService as a dependency to TodosComponent. 
-// 
+// because the test will fail. This is because we have not
+// provided the TodoService as a dependency to TodosComponent.
+//
 // When you get to Lecture 6 (Providing Dependencies), be sure
-// to remove "x" from "xdescribe" below. 
+// to remove "x" from "xdescribe" below.
 
-xdescribe('TodosComponent', () => {
+describe('TodosComponent', () => {
   let component: TodosComponent;
   let fixture: ComponentFixture<TodosComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ TodosComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientModule],
+      declarations: [TodosComponent],
+      providers: [TodoService],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
