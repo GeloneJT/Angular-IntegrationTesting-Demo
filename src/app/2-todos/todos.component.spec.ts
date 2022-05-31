4729249a08bb9ffcbe14fe5d4415dcc5d0b8370e
Jelani <eyeglass0324@gmail.com>
2022-05-31 16:00:14 -0400

    todos component dependency demo completed

--- src/app/2-todos/todos.component.spec.ts
+++ src/app/2-todos/todos.component.spec.ts
@@ -1,11 +1,10 @@
 /* tslint:disable:no-unused-variable */
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
+import { from } from 'rxjs';
 
 //NOTE: I've deliberately excluded this suite from running
 // because the test will fail. This is because we have not
@@ -29,10 +28,15 @@ describe('TodosComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TodosComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should load todos from the server', () => {
+    let service = TestBed.inject(TodoService);
+
+    spyOn(service, 'getTodos').and.returnValue(from([[1, 2, 3]]));
+
+    fixture.detectChanges();
+
+    expect(component.todos.length).toBe(3);
   });
 });
